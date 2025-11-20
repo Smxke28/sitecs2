0d7f5ef17a5ba2ebcfbce1a340d6400701c3c423
Smxke28 <juanlavecchia23@gmail.com>
2025-11-20 14:26:05 -0300

    feat: descrição curta da mudança

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  theme: { extend: {} },
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-}
+};
